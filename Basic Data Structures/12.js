1996e9b1990baceaf8ba513c230ae8c57a16a6be
ToshaBY <anton.rybakou@gmail.com>
2022-05-13 21:54:33 +0300

    feat: add 12 solution

---
+++ Basic Data Structures/12.js
@@ -0,0 +1,20 @@
+let myNestedArray = [
+    // Only change code below this line
+    'unshift',
+    false,
+    1,
+    2,
+    3,
+    'complex',
+    'nested',
+    [
+        [
+            'deep',
+            [
+                'deeper',
+                ['deepest']
+            ]
+        ]
+    ]
+    // Only change code above this line
+];
