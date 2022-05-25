600830121a56320acb87c3434dadc233a4cea6ce
ToshaBY <anton.rybakou@gmail.com>
2022-05-25 23:33:44 +0300

    feat: add 16 solution

---
+++ Basic Algorithm Scripting/16.js
@@ -0,0 +1,14 @@
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns
+// them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+    let i = 0;
+    while (i < arr.length) {
+        newArr.push(arr.slice(i, i + size));
+        i += size;
+    }
+    return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
